b43c62fbc4543001e2be97d90079f7961b55f431
Joshua Tapp <joshua.c.tapp@icloud.com>
2019-04-11 18:05:50 -0400

    Updated typos, modified comments to better describe behavior, wrote discussion for this week on Sets vs List uses.
    Signed-off-by: Joshua Tapp <joshua.c.tapp@icloud.com>

--- src/chapter21/SetOperationsOnLinkedHashSets.java
+++ src/chapter21/SetOperationsOnLinkedHashSets.java
@@ -1,5 +1,5 @@
 /*
- * Created on 4/10/19 10:58 PM by Joshua Tapp
+ * Created on 4/11/19 6:05 PM by Joshua Tapp
  * Student at John Tyler Community College
  */
 
@@ -18,24 +18,18 @@ public class SetOperationsOnLinkedHashSets {
 
     public static void main(String[] args){
 
-        //Create two Sets that contain String arrays
+        //Arrays.asList takes an array and returns it as a List. Because List/Sets are convertible, a list can construct a Set.
         LinkedHashSet<String> set1 = new LinkedHashSet<>(Arrays.asList("George", "Jim", "John", "Blake", "Kevin", "Michael"));
         LinkedHashSet<String> set2 = new LinkedHashSet<>(Arrays.asList("George", "Katie", "Kevin", "Michelle", "Ryan"));
 
-        /*Set Interface Bulk Operations
-            .addAll combines two sets
-            .removeAll removes matching values
-            .retail all removes but matching values
-         */
+        LinkedHashSet<String> union = new LinkedHashSet<>(set1);        // Due to data destruction, copy set1 to union.
+        union.addAll(set2);                                             // Adds all elements to the union Set.
 
-        LinkedHashSet<String> union = new LinkedHashSet<>(set1);
-        union.addAll(set2);
+        LinkedHashSet<String> difference = new LinkedHashSet<>(set1);   // Due to data destruction, copy set1 to difference.
+        difference.removeAll(set2);                                     // Because sets can not have duplicate elements, this method removes all matching elements from the two sets
 
-        LinkedHashSet<String> difference = new LinkedHashSet<>(set1);
-        difference.removeAll(set2);
-
-        LinkedHashSet<String> intersection = new LinkedHashSet<>(set1);
-        intersection.retainAll(set2);
+        LinkedHashSet<String> intersection = new LinkedHashSet<>(set1); // Due to data destruction, copy set1 to intersection.
+        intersection.retainAll(set2);                                   // Sets retains all duplicates and removes all distinct elements.
 
         System.out.println("The union of the two sets: " + union + "\n");
         System.out.println("The difference of the two sets: " + difference +"\n");
